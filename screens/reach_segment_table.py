298e81548cdc2bac04122d99fd8b2a2c91c234ec
Joshua <xeon826@gmail.com>
2024-09-03 20:56:09 -0500

    reach segment table

--- screens/reach_segment_table.py
+++ screens/reach_segment_table.py
@@ -1,12 +1,13 @@
 from kivy.metrics import dp
-
-from kivymd.app import MDApp
 from kivymd.uix.datatables import MDDataTable
 from kivymd.uix.screen import MDScreen
 
 
-class ReachSegmentTable(MDApp):
-    def build(self):
+class ReachSegmentTable(MDScreen):
+    def __init__(self, **kwargs):
+        super(ReachSegmentTable, self).__init__(**kwargs)
+
+        # Create the data table
         self.data_tables = MDDataTable(
             use_pagination=True,
             check=True,
@@ -82,33 +83,23 @@ class ReachSegmentTable(MDApp):
             sorted_order="ASC",
             elevation=2,
         )
+        
+        # Bind events for row press and check press
         self.data_tables.bind(on_row_press=self.on_row_press)
         self.data_tables.bind(on_check_press=self.on_check_press)
-        screen = MDScreen()
-        screen.add_widget(self.data_tables)
-        return screen
+
+        # Add the data table to the screen
+        self.add_widget(self.data_tables)
 
     def on_row_press(self, instance_table, instance_row):
-        '''Called when a table row is clicked.'''
-
+        """Called when a table row is clicked."""
         print(instance_table, instance_row)
 
     def on_check_press(self, instance_table, current_row):
-        '''Called when the check box in the table row is checked.'''
-
+        """Called when the check box in the table row is checked."""
         print(instance_table, current_row)
 
-    # Sorting Methods:
-    # since the https://github.com/kivymd/KivyMD/pull/914 request, the
-    # sorting method requires you to sort out the indexes of each data value
-    # for the support of selections.
-    #
-    # The most common method to do this is with the use of the builtin function
-    # zip and enumerate, see the example below for more info.
-    #
-    # The result given by these funcitons must be a list in the format of
-    # [Indexes, Sorted_Row_Data]
-
+    # Sorting methods for the data table
     def sort_on_signal(self, data):
         return zip(*sorted(enumerate(data), key=lambda l: l[1][2]))
 
@@ -127,3 +118,4 @@ class ReachSegmentTable(MDApp):
 
     def sort_on_team(self, data):
         return zip(*sorted(enumerate(data), key=lambda l: l[1][-1]))
+
