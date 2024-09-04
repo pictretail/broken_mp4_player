18f710a3541294dfcec88c282e679d548a4cfc46
Joshua <xeon826@gmail.com>
2024-09-03 20:07:11 -0500

    file structure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
-from screens.login_screen import LoginScreen
-from screens.file_picker_screen import FilePickerScreen
+from screens.login.login_screen import LoginScreen
+from screens.filepicker.file_picker_screen import FilePickerScreen
 
 # Create the main application class
 class FilePickerApp(App):
